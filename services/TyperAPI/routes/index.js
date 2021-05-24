9a04879fb87dacc5ea726a9f5d81afd4914fb949
amptxs <72046996+amptxs@users.noreply.github.com>
2021-05-25 01:57:17 +0500

    В результатах добавлены опечатки и скорость, фильтр по опечаткам и скорости

--- services/TyperAPI/routes/index.js
+++ services/TyperAPI/routes/index.js
@@ -20,15 +20,19 @@ router.get('/', function(req, res, next) {
 //JSON POST body format:
 //{
 // "name":"test",
-// "score":10
+// "score":250,
+// "misprints": 0,
+// "rate": 113
 // }
 router.post('/share', function (req, res)
 {
   console.log("Name: " + req.body.name);
   console.log("Score: " + req.body.score);
-
-  let user = {Name: req.body.name, Score: req.body.score, Date: new Date()};
-  collection.insertOne(user);
+  console.log("Misprints: " + req.body.misprints);
+  console.log("Rate: " + req.body.rate);
+  let result = {Name: req.body.name, Score: req.body.score,
+    Misprints: req.body.misprints, Rate: req.body.rate , Date: new Date()};
+  collection.insertOne(result);
 
   res.send("Success");
 });
@@ -66,6 +70,26 @@ router.post('/resultsScoreFilter', function (req, res)
   scoreFilter(req.body.score, res);
 });
 
+//Фильтр по ошибкам.
+//JSON POST body format:
+//{
+// "misprints": 10 <- (вернет результаты где ошибок больше 10)
+// }
+router.post('/resultsMisprintsFilter', function (req, res)
+{
+  misprintsFilter(req.body.misprints, res);
+});
+
+//Фильтр по скорости.
+//JSON POST body format:
+//{
+// "rate": 100 <- (вернет результаты где скорость больше 100)
+// }
+router.post('/resultsRateFilter', function (req, res)
+{
+  rateFilter(req.body.rate, res);
+});
+
 function dateFilter(amount, unit, res) {
   var dateTimeTofilter = moment().subtract(amount, unit);
   var filter = {
@@ -79,8 +103,19 @@ function dateFilter(amount, unit, res) {
 }
 
 function scoreFilter(score, res) {
-
   let filter = { Score: { $gt: score } };
     collection.find(filter).sort({"Score": -1}).toArray(function(err, result) {
       res.send(result)});
+}
+
+function misprintsFilter(misprints, res) {
+  let filter = { Misprints: { $gt: misprints } };
+  collection.find(filter).sort({"Misprints": -1}).toArray(function(err, result) {
+    res.send(result)});
+}
+
+function rateFilter(rate, res) {
+  let filter = { Rate: { $gt: rate } };
+  collection.find(filter).sort({"Rate": -1}).toArray(function(err, result) {
+    res.send(result)});
 }
